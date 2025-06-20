b0e6bbe073aba9b584ea643873225cc1a78bb03f
Sandun Dissanayake <2020t00864@stu.cmb.ac.lk>
2025-06-20 19:44:59 +0530

    bottom nav

--- app/src/main/java/com/example/fotnewsapp/MainActivity.java
+++ app/src/main/java/com/example/fotnewsapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
